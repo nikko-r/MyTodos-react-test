69a7ca011afa9e4ef31f61cc29d015c980b1b6cc
nikko <nikkorivas@outlook.com>
2022-07-28 15:13:20 +0100

    Added click even for button

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -1,14 +1,24 @@
 import "./InputBox.scss";
 
-const InputBox = () => {
+const InputBox = ({ InputString }) => {
+  const handleAddTaskClick = () => {
+    console.log("click");
+    console.log({ InputString });
+  };
+
+  // const handleInput = (event) => {
+  //   setSearchBoxInput(event.target.value);
+  // };
+
   return (
     <div className="input-box">
       <input
         className="input-box__input"
         type="text"
         placeholder="Add your task here..."
+        value={InputString}
       ></input>
-      <button className="input-box__add-button">
+      <button onClick={handleAddTaskClick} className="input-box__add-button">
         <div className="add-button__circle">+</div>
       </button>
     </div>
