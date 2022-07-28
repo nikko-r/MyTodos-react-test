46e75d1b809e77504591adbfac19c874722ed8ef
nikko <nikkorivas@outlook.com>
2022-07-28 11:16:23 +0100

    Added basic scss for top section and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="top-section">
+        <h1 className="top-section__title">My Todos</h1>
+        <button className="top-section__reset-button">Reset</button>
+      </div>
+      <SearchBar></SearchBar>
+    </div>
+  );
 }
 
 export default App;
